d43f97a83f1e77c91d99e55e45f47853427ecb45
Sidneyffg <sidney.oostveen@gmail.com>
2023-12-12 19:49:39 +0100

    fixed backup page not working

--- website/backups/backups.js
+++ website/backups/backups.js
@@ -1,72 +1,74 @@
-function run() {
-  socket.on(
-    `automaticBackupSettingsUpdate${utils.server.num}`,
-    (newSettings) => {
-      closeEditPopup();
+const main = {
+  start() {
+    socket.on(
+      `automaticBackupSettingsUpdate${utils.server.num}`,
+      (newSettings) => {
+        closeEditPopup();
 
-      for (const e in newSettings)
-        if (newSettings[e] != -1) newSettings[e] /= 36e5;
+        for (const e in newSettings)
+          if (newSettings[e] != -1) newSettings[e] /= 36e5;
 
-      const automaticBackup = newSettings.timeBetweenAutomaticBackups !== -1;
-      automaticBackupsSwitch.checked = automaticBackup;
-      deleteBackupTimeInp.value =
-        newSettings.deleteAutomaticBackupAfter == -1
-          ? ""
-          : newSettings.deleteAutomaticBackupAfter;
-      if (automaticBackup) {
-        backupTimeInp.value = newSettings.timeBetweenAutomaticBackups;
+        const automaticBackup = newSettings.timeBetweenAutomaticBackups !== -1;
+        automaticBackupsSwitch.checked = automaticBackup;
+        deleteBackupTimeInp.value =
+          newSettings.deleteAutomaticBackupAfter == -1
+            ? ""
+            : newSettings.deleteAutomaticBackupAfter;
+        if (automaticBackup) {
+          backupTimeInp.value = newSettings.timeBetweenAutomaticBackups;
 
-        openAutomaticBackupsContent();
-      } else {
-        closeAutomaticBackupsContent();
-        backupTimeInp.value = "1";
+          openAutomaticBackupsContent();
+        } else {
+          closeAutomaticBackupsContent();
+          backupTimeInp.value = "1";
+        }
+
+        stdBackupData = getBackupData();
       }
+    );
 
-      stdBackupData = getBackupData();
-    }
-  );
-
-  socket.on(`backupUpdate${utils.server.num}`, (backups) => {
-    let backupHtml = `
-    <div>
-      <h3>Backups</h3>
-      <button onclick="createBackup()">Create backup</button>
-    </div>`;
-
-    const date = new Date();
-    const year = date.getFullYear();
-
-    backups.forEach((backup, idx) => {
-      const backupDate = new Date(backup.timestamp);
-      const minutes = "0" + backupDate.getMinutes();
-      backupHtml += `
-      <div class="backup-item">
-        <div>
-          <p>${backupDate.getDate()} ${months[backupDate.getMonth()]} ${
-        backupDate.getFullYear() == year ? "" : backupDate.getFullYear()
-      }</p>
-          <p>${backupDate.getHours()}:${minutes.substring(
-        minutes.length - 2,
-        minutes.length
-      )}</p>
-        </div>
-        <input id="backup-item-checkbox-${idx}" type="checkbox" style="display: none;">
-        <label for="backup-item-checkbox-${idx}">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-            <path fill="white"
-                d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
-          </svg>
-        </label>
-        <div class="list">
-          <button onclick="restoreBackup('${backup.id}')">Restore</button>
-          <button onclick="deleteBackup('${backup.id}')">Delete</button>
-        </div>
+    socket.on(`backupUpdate${utils.server.num}`, (backups) => {
+      let backupHtml = `
+      <div>
+        <h3>Backups</h3>
+        <button onclick="createBackup()">Create backup</button>
       </div>`;
+
+      const date = new Date();
+      const year = date.getFullYear();
+
+      backups.forEach((backup, idx) => {
+        const backupDate = new Date(backup.timestamp);
+        const minutes = "0" + backupDate.getMinutes();
+        backupHtml += `
+        <div class="backup-item">
+          <div>
+            <p>${backupDate.getDate()} ${months[backupDate.getMonth()]} ${
+          backupDate.getFullYear() == year ? "" : backupDate.getFullYear()
+        }</p>
+            <p>${backupDate.getHours()}:${minutes.substring(
+          minutes.length - 2,
+          minutes.length
+        )}</p>
+          </div>
+          <input id="backup-item-checkbox-${idx}" type="checkbox" style="display: none;">
+          <label for="backup-item-checkbox-${idx}">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+              <path fill="white"
+                  d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
+            </svg>
+          </label>
+          <div class="list">
+            <button onclick="restoreBackup('${backup.id}')">Restore</button>
+            <button onclick="deleteBackup('${backup.id}')">Delete</button>
+          </div>
+        </div>`;
+      });
+      document.getElementById("server-backups-list").innerHTML = backupHtml;
     });
-    document.getElementById("server-backups-list").innerHTML = backupHtml;
-  });
-  utils.stopMenu.init();
-}
+    utils.stopMenu.init();
+  },
+};
 
 const getById = (id) => document.getElementById(id);
 const automaticBackupsContent = getById("automatic-backups-content");
