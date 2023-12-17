01c755144315e7b9c1ff70a273b22efe101abf4d
Sidneyffg <sidney.oostveen@gmail.com>
2023-12-17 09:50:22 +0100

    rewrote server list overview javascript into object to function with utils

--- website/index.js
+++ website/index.js
@@ -1,12 +1,32 @@
-const darkener = document.getElementById("darkener"),
-  newServerMenu = document.getElementById("std-menu");
-
-function closeNewServer() {
-  darkener.style.display = "none";
-  newServerMenu.style.display = "none";
-}
-
-function openNewServer() {
-  darkener.style.display = "block";
-  newServerMenu.style.display = "block";
-}
+const main = {
+  start() {
+    this.elems = document.getElementsByClassName("server-status-color");
+    for (let i = 0; i < this.elems.length; i++) {
+      socket.on("statusUpdate" + i, (status) => this.changeStatus(i, status));
+    }
+    socket.on("statusUpdate" + this.elems.length, () => location.reload());
+  },
+  darkener: document.getElementById("darkener"),
+  newServerMenu: document.getElementById("std-menu"),
+  elems: null,
+  closeNewServer() {
+    this.darkener.style.display = "none";
+    this.newServerMenu.style.display = "none";
+  },
+  openNewServer() {
+    this.darkener.style.display = "block";
+    this.newServerMenu.style.display = "block";
+  },
+  changeStatus(num, status) {
+    this.elems[num].style.backgroundColor = this.statusToColor(status);
+  },
+  statusToColor(s) {
+    return [
+      { s: "online", c: "lime" },
+      { s: "offline", c: "grey" },
+      { s: "starting", c: "cyan" },
+      { s: "stopping", c: "cyan" },
+      { s: "downloading", c: "yellow" },
+    ].find((e) => e.s == s).c;
+  },
+};
